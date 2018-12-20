98cceb27f5c55a74a5ea6e940c8a96017697dd89
lei314121077 <ldx314121077@163.com>
2018-12-20 01:45:13 -0800

    create transfer

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-package data_transfer_chaincode
+package main
 
 import (
 	"fmt"
@@ -8,49 +8,60 @@ import (
 
 type DataExchangeCenter struct{}
 
-/*
-@name init chaincode
- */
+//TODO
 func (d *DataExchangeCenter) Init(stub shim.ChaincodeStubInterface) pb.Response {
-
-
 	return shim.Success(nil)
 }
 
-/*
-@name invoke chaincode
-*/
+//TODO
 func (d *DataExchangeCenter) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
 
 	fn, args := stub.GetFunctionAndParameters()
 	fmt.Sprintf("invoke 正在运行:%s " , fn)
 
 	switch fn {
-		//返回调用者信息
-		case "":
-			return d.addData(stub, args)
-		default:
+
+	//添加交易信息
+	case "CreateTransferApplication":
+		return d.CreateTransferApplication(stub, args)
+
+	default:
 			fmt.Println("invoke 未找到函数名为: " + fn)
 			return shim.Error(fmt.Sprintf("Invalid invoke function name...%s",fn ))
 	}
 
 }
 
+
 /*
-@name adddata
+@name 将数据存储到交易账本中
 */
-func (d *DataExchangeCenter)addData(stub shim.ChaincodeStubInterface, args []string)pb.Response{
+func (d *DataExchangeCenter)CreateTransferApplication(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	fmt.Println("Entering CreateTransferApplication")
 
-	return shim.Error("...")
+	if len(args) < 2 {
+		fmt.Println("args参数错误！< 2")
+		return shim.Error("交易数据参数错误！")
+	}
+
+	var transferApplicationId, transferApplicationInput = args[0],args[1]
+	err := stub.PutState(transferApplicationId, []byte(transferApplicationInput))
+	if err != nil {
+		fmt.Println("无法保存交易数据：", err)
+		return shim.Error(fmt.Sprintf("保存交易数据失败！", err))
+	}
+
+	fmt.Println("保存交易数据成功")
+	//TODO 未确定返回的格式
+	return shim.Success(nil)
 }
-
-
 
 
 func main() {
 	if err := shim.Start(new(DataExchangeCenter)); err != nil {
 		fmt.Printf("启动链码失败...！错误: %s", err)
 	}
+	fmt.Println("Chaincode成功启动!")
 }
 
 
