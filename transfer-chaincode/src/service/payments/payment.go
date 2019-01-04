4b77dce3ee0c4c8b7b48de2da70fd2bd9a70b2bd
lei314121077 <ldx314121077@163.com>
2019-01-04 00:17:45 -0800

    ca

--- transfer-chaincode/src/service/payments/payment.go
+++ transfer-chaincode/src/service/payments/payment.go
@@ -6,9 +6,52 @@ import (
 	"strconv"
 )
 
+
+// 向指定的账户存入对应的金额
+// -c '{"Args":["set", "账户名称", "要存入的金额"]}'
+func (p *Payment) set(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+
+	if len(args) != 2 {
+		return shim.Error("必须且只能指定账户名称及要存入的金额")
+	}
+
+	result, err := stub.GetState(args[0])
+	if err != nil {
+		return shim.Error("根据指定的账户查询信息失败")
+	}
+
+	if result == nil {
+		return shim.Error("指定的账户不存在")
+	}
+
+	// 存入账户
+	val, err := strconv.Atoi(string(result))
+	if err != nil {
+		return shim.Error("处理指定的账户金额时发生错误")
+	}
+
+	x, err := strconv.Atoi(args[1])
+	if err != nil {
+		return shim.Error("指定要存入的金额错误")
+	}
+
+	val = val + x
+
+	// 保存信息
+	err = stub.PutState(args[0], []byte(strconv.Itoa(val)))
+	if err != nil {
+		return shim.Error("存入账户金额时发生错误")
+	}
+
+	return shim.Success([]byte("存入操作成功"))
+
+}
+
+
+
 // 转账
 // -c '{"Args":["Payment", "源账户名称", "目标账户名称", "转账金额"]}'
-func (u *Payment)PaymentTransfer(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+func (p *Payment)PaymentTransfer(stub shim.ChaincodeStubInterface, args []string) pb.Response {
 
 	if len(args) != 3 {
 		return shim.Error("必须且只能指定源账户及目标账户名称与对应的转账金额")
