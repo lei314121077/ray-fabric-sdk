ac783c73bee70d9195ccb0bb7a1d00111206d5bc
lei314121077 <ldx314121077@163.com>
2019-01-08 00:00:05 -0800

    rabbitmq tool

--- fabricsdk-api/src/services/demo/demo_test.go
+++ fabricsdk-api/src/services/demo/demo_test.go
@@ -1,13 +1,60 @@
 package demo
 
-import "testing"
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+
+var (
+	level1 = 10000		//一万级
+	level2 = 100000		//十万级
+	level3 = 1000000	//百万级
+	level4 = 10000000	//千万级
+)
 
 func TestApplication_DemoApi(t *testing.T) {
+
+	start := time.Now()
+	var wg sync.WaitGroup
+	for i:=0; i<=100000; i++{
+
+		go func(){
+
+
+			wg.Done()
+		}()
+
+	}
+
+	wg.Wait()
+
+	fmt.Println("开始时间为：", start)
+	fmt.Println("耗时为：", int64( time.Since(start).Nanoseconds()))
 
 }
 
 
 func TestServiceSetup_DemoSer(t *testing.T) {
+
+	start := time.Now()
+	var wg sync.WaitGroup
+	for i:=0; i<=100000; i++{
+
+		go func(){
+
+
+			wg.Done()
+		}()
+
+	}
+
+	wg.Wait()
+
+	fmt.Println("开始时间为：", start)
+	fmt.Println("耗时为：", int64( time.Since(start).Nanoseconds()))
 
 }
 
--- fabricsdk-api/src/services/user/user_test.go
+++ fabricsdk-api/src/services/user/user_test.go
@@ -1,13 +1,58 @@
 package user
 
-import "testing"
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
 
+var (
+	level1 = 10000		//一万级
+	level2 = 100000		//十万级
+	level3 = 1000000	//百万级
+	level4 = 10000000	//千万级
+)
 
 func TestUser_UserDemo(t *testing.T) {
+
+	start := time.Now()
+	var wg sync.WaitGroup
+	for i:=0; i<=100000; i++{
+
+		go func(){
+
+
+			wg.Done()
+		}()
+
+	}
+
+	wg.Wait()
+
+	fmt.Println("开始时间为：", start)
+	fmt.Println("耗时为：", int64( time.Since(start).Nanoseconds()))
 
 }
 
 func TestUser_UserDemoSer(t *testing.T) {
+
+	start := time.Now()
+	var wg sync.WaitGroup
+	for i:=0; i<=100000; i++{
+
+		go func(){
+
+
+			wg.Done()
+		}()
+
+	}
+
+	wg.Wait()
+
+	fmt.Println("开始时间为：", start)
+	fmt.Println("耗时为：", int64( time.Since(start).Nanoseconds()))
 
 }
 
