78228ae0de555ac74df122003f580011318cccb5
leidongxing <leidx@bmkcrypto.com>
2019-01-27 23:44:14 -0800

    open tls

--- fabricsdk-api/src/httpsdk/httpgateway.go
+++ fabricsdk-api/src/httpsdk/httpgateway.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"net/http"
 	"order"
+	"time"
 )
 
 
@@ -60,31 +61,31 @@ func HttpStart(){
 	router.HandleFunc("/quseryHistoryApi", o.QueryHistoryApi).Methods("POST")
 
 	// tls验证
-	//cfg := &tls.Config{
-	//	MinVersion:               tls.VersionTLS12,
-	//	CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
-	//	PreferServerCipherSuites: true,
-	//	CipherSuites: []uint16{
-	//		tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-	//		tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
-	//		tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
-	//		tls.TLS_RSA_WITH_AES_256_CBC_SHA,
-	//	},
-	//}
-	//
-	//srv := &http.Server{
-	//	Addr:         *addr,
-	//	WriteTimeout: time.Second * 15,
-	//	ReadTimeout:  time.Second * 15,
-	//	IdleTimeout:  time.Second * 60,
-	//	Handler: router, // Pass our instance of gorilla/mux in.
-	//	TLSConfig:    cfg,
-	//	TLSNextProto: make(map[string]func(*http.Server, *tls.Conn, http.Handler), 0),
-	//}
+	cfg := &tls.Config{
+		MinVersion:               tls.VersionTLS12,
+		CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+		PreferServerCipherSuites: true,
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+		},
+	}
+
+	srv := &http.Server{
+		Addr:         *addr,
+		WriteTimeout: time.Second * 15,
+		ReadTimeout:  time.Second * 15,
+		IdleTimeout:  time.Second * 60,
+		Handler: router, // Pass our instance of gorilla/mux in.
+		TLSConfig:    cfg,
+		TLSNextProto: make(map[string]func(*http.Server, *tls.Conn, http.Handler), 0),
+	}
 	fmt.Println("ok start successfull !")
 
 	log.Fatal(http.ListenAndServe(*addr, router))
-	//log.Fatal(srv.ListenAndServeTLS("./server.rsa.crt", "./server.rsa.key"))
+	log.Fatal(srv.ListenAndServeTLS("./server.rsa.crt", "./server.rsa.key"))
 
 }
 
